2236a8651461b7bc1df872ed55083f222b92827b
Brandon Anzaldi <brandon@brandonanzaldi.com>
2014-05-24 16:15:21 -0700

    New commands

---
+++ commands.js
@@ -0,0 +1,15 @@
+var commands = function (input) {
+	var inParts = input.split(' ');
+	var cmd = inParts[0]
+	if(cmd === 'ls' || cmd === 'dir') {
+		// List directory
+	} else if(cmd === 'cd') {
+		// Change Directory
+	} else if(cmd === 'dl') {
+		// Download File
+	} else if(cmd === 'exit' || cmd === quit) {
+		// Quit
+	} else if(cmd === 'help' || cmd === '?') {
+		// Help
+	}
+}
