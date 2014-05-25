7c7f45550ec31a456420e6bdd4ee3ec285a8be91
Dash Scarborough <dash@dash-ThinkPad-X61-Tablet.(none)>
2014-05-25 04:26:45 -0700

    did lotso shizzle, ls beginings, localized jquery

--- public/js/commands.js
+++ public/js/commands.js
@@ -25,7 +25,8 @@ var commands = function (input, cb) {
 	var inParts = input.split(' ');
 	var cmd = inParts[0];
 	if(cmd === 'ls' || cmd === 'dir') {
-		// List directory
+		var lsr = ls(inParts[1]);
+        return cb(lsr[0], lsr[1]);
 	} else if(cmd === 'cd') {
 		var cdr = cd(inParts[1]);
         return cb(cdr[0], cdr[1]);
@@ -50,12 +51,17 @@ var commands = function (input, cb) {
 var help = function(cb) {
 	var info = " ls - List files in current directory.\n \
 cd <dir> - Change to directory <dir>.\n \
-dl <file> - Download file <file> from remote server.\n \
+wget <file> - Download file <file> from remote server.\n \
 help - This.\n \
 mail - Read your mail.\n \
 cat - Output the contents of a file.\n \
 exit - Quit the terminal.";
 	return info;
+}
+
+var ls = function(dir) {
+    var ret = fileStructure[dir].children;
+    return [null, ret];
 }
 
 var cd = function(newDir) {
